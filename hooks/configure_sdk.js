e57bf30f8edb30586f47b5c877bcf7c4ad09b631
Stein Welberg <stein@onegini.com>
2016-07-28 09:49:31 +0200

    CORDOPLUG-94 Use separate config ZIP's for Android and iOS and add more logging

--- hooks/configure_sdk.js
+++ hooks/configure_sdk.js
@@ -1,23 +1,34 @@
+'use strict';
+
 const fs = require('fs');
 const spawn = require('child_process').spawn;
 
 module.exports = function (context) {
-  const configPath = context.opts.projectRoot + "/onegini-config.zip";
   const args = [
     '--cordova',
-    '--config', configPath,
     '--app-dir', context.opts.projectRoot
   ];
-  console.log(`Configuring Onegini SDK`);
+  console.log('Configuring the Onegini SDK');
+  console.log('===========================');
+  console.log('')
+  console.log('')
 
   context.opts.platforms.forEach((platform) => {
     let platformArgs = args;
     platformArgs.unshift(platform);
+    platformArgs.push('--config',`${context.opts.projectRoot}/onegini-config-${platform}.zip`)
+
+    console.log(`Configuring the ${platform} platform`)
+    console.log('--------------------------' + Array(platform.length).join("-"))
+    console.log('')
     execConfigurator(platformArgs);
   });
 };
 
 function execConfigurator(args) {
+  console.log('Running command: ')
+  console.log('onegini-sdk-configurator ' + args.join(' '))
+  console.log('')
   const configurator = spawn('onegini-sdk-configurator', args);
 
   configurator.stdout.on('data', (data) => {
@@ -29,8 +40,8 @@ function execConfigurator(args) {
   });
 
   configurator.on('close', (code) => {
-    if(code !== 0) {
-      console.log(`onegini-cordova-plugin: Could not configure Onegini SDK with your configuration`);
+    if (code !== 0) {
+      console.log('onegini-cordova-plugin: Could not configure Onegini SDK with your configuration');
       process.exit(code);
     }
   });
