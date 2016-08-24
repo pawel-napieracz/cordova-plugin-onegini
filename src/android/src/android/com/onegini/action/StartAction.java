013a6b00662cf79de8c43a1a83be70e2620804f4
Jim van der Voort <jimvandervoort@me.com>
2016-08-24 11:15:28 +0200

    CORDOPLUG-121 Implement CordovaClient.start on Android

--- src/android/src/android/com/onegini/action/StartAction.java
+++ src/android/src/android/com/onegini/action/StartAction.java
@@ -4,14 +4,12 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 
 import com.onegini.OneginiCordovaPlugin;
-import com.onegini.OneginiSDK;
 import com.onegini.handler.InitializationHandler;
 
 public class StartAction implements OneginiPluginAction {
 
   @Override
   public void execute(final JSONArray args, final CallbackContext callbackContext, final OneginiCordovaPlugin client) {
-    OneginiSDK.getOneginiClient(client.getCordova().getActivity().getApplicationContext());
     client.getOneginiClient().start(new InitializationHandler(callbackContext));
   }
 }
--- src/android/src/android/com/onegini/handler/PinAuthenticationRequestHandler.java
+++ src/android/src/android/com/onegini/handler/PinAuthenticationRequestHandler.java
@@ -1,5 +1,6 @@
 package com.onegini.handler;
 
+import android.util.Log;
 import com.onegini.mobile.android.sdk.handlers.request.OneginiPinAuthenticationRequestHandler;
 import com.onegini.mobile.android.sdk.handlers.request.callback.OneginiPinCallback;
 import com.onegini.mobile.android.sdk.model.entity.UserProfile;
@@ -8,16 +9,16 @@ public class PinAuthenticationRequestHandler implements OneginiPinAuthentication
 
   @Override
   public void startAuthentication(final UserProfile userProfile, final OneginiPinCallback oneginiPinCallback) {
-
+    Log.v("OneginiCordovaPlugin", "startAuthentication");
   }
 
   @Override
   public void onNextAuthenticationAttempt(final int i, final int i1) {
-
+    Log.v("OneginiCordovaPlugin", "onNextAuthenticationAttempt");
   }
 
   @Override
   public void finishAuthentication() {
-
+    Log.v("OneginiCordovaPlugin", "finishAuthentication");
   }
 }
