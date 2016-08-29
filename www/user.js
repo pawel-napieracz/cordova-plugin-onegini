f7b043edc9e97cb9e8c815f7c3b3e8aa044f0eb9
Jim van der Voort <jimvandervoort@me.com>
2016-08-29 16:42:16 +0200

    CORDOPLUG-124 Update JS API to user new registerUser actions.

--- www/user.js
+++ www/user.js
@@ -2,11 +2,14 @@ module.exports = (function () {
   var utils = require('./utils');
 
   function startRegistration(options, successCb, failureCb) {
-    return utils.promiseOrCallbackExec('OneginiUserRegistrationClient', 'startRegistration', options, successCb, failureCb);
+    return utils.promiseOrCallbackExec('OneginiUserClient', 'startRegistration', options.scopes, successCb, failureCb);
   }
 
   function createPIN(options, successCb, failureCb) {
-    return utils.promiseOrCallbackExec('OneginiUserRegistrationClient', 'createPIN', options, successCb, failureCb);
+    if (!successCb) {
+      throw new TypeError("Onegini: missing argument for createPIN: no success callback provided");
+    }
+    utils.promiseOrCallbackExec('OneginiUserClient', 'createPIN', options.pin, successCb, failureCb);
   }
 
   return {
