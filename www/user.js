66a7f47f9f0634ed5a1f271691969c58a9694282
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-08-30 14:52:35 +0200

    passing in the entire object, for future compatibility sake

--- www/user.js
+++ www/user.js
@@ -2,7 +2,7 @@ module.exports = (function () {
   var utils = require('./utils');
 
   function startRegistration(options, successCb, failureCb) {
-    return utils.promiseOrCallbackExec('OneginiUserRegistrationClient', 'startRegistration', options.scopes, successCb, failureCb);
+    return utils.promiseOrCallbackExec('OneginiUserRegistrationClient', 'startRegistration', options, successCb, failureCb);
   }
 
   function createPin(options, successCb, failureCb) {
@@ -10,7 +10,7 @@ module.exports = (function () {
       throw new TypeError("Onegini: missing argument for createPin: no success callback provided");
     }
     // no promise possible as this may be called more than once
-    utils.promiseOrCallbackExec('OneginiUserRegistrationClient', 'createPin', options.pin, successCb, failureCb);
+    utils.promiseOrCallbackExec('OneginiUserRegistrationClient', 'createPin', options, successCb, failureCb);
   }
 
   function getUserProfiles(successCb, failureCb) {
