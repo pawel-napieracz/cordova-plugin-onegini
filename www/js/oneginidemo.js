bf83f21b0e3d347b18c13e7a2417e58e8501e71c
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-08-30 11:08:06 +0200

    Added 'getRegisteredUsers'

--- www/js/oneginidemo.js
+++ www/js/oneginidemo.js
@@ -1,56 +1,67 @@
-var OneginiDemo = (function() {
+var OneginiDemo = (function () {
   return {
-    start: function() {
+    start: function () {
       onegini.start(
-          function() {
+          function () {
             console.log("Onegini is ready to go!");
           },
-          function(err) {
+          function (err) {
             alert("Error!\n\n" + err.description);
           }
       );
     },
 
-    isRegistered: function() {
+    isRegistered: function () {
       onegini.user.isRegistered(
-        function(registered) {
-          alert(registered ? "YES" : "NO");
-        },
-        function(err) {
-          alert("Error!\n\n" + err.description);
-        }
+          function (registered) {
+            alert(registered ? "YES" : "NO");
+          },
+          function (err) {
+            alert("Error!\n\n" + err.description);
+          }
       );
     },
 
-    startRegistration: function() {
+    startRegistration: function () {
       var that = this;
       onegini.user.startRegistration(
           {
             scopes: ["read"]
           },
-          function(result) {
+          function (result) {
             that.pinLength = result.pinLength;
             // added a little timeout so the embedded browser has time to disappear
-            setTimeout(function() {
+            setTimeout(function () {
               alert("Success!\n\n" + JSON.stringify(result));
-            }, 300);
+            }, 900);
           },
-          function(err) {
+          function (err) {
             alert("Error!\n\n" + err.description);
           }
       );
     },
 
-    createPIN: function() {
+    createPIN: function () {
       var pin = prompt("Please enter your " + this.pinLength + " digit PIN", "12346" /* default */);
       onegini.user.createPIN(
           {
             pin: pin
           },
-          function(result) {
+          function (result) {
             alert("Success!\n\nProfile created: " + result.profileId);
           },
-          function(err) {
+          function (err) {
+            alert("Error!\n\n" + err.description);
+          }
+      );
+    },
+
+    getRegisteredUsers: function () {
+      onegini.user.getRegisteredUsers(
+          function (result) {
+            alert("Success!\n\Registered Users:\n" + JSON.stringify(result));
+          },
+          function (err) {
             alert("Error!\n\n" + err.description);
           }
       );
