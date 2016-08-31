9d7150fd6ebab336496045f7e19261880ec8df17
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-08-31 10:21:59 +0200

    CORDOPLUG-141 Add Cordova plugin test framework for on-device testing - added more checks, the rest will need to be done during userstory dev

--- tests/tests.js
+++ tests/tests.js
@@ -6,8 +6,10 @@ exports.defineAutoTests = function () {
       expect(window.onegini).toBeDefined();
     });
 
-    it("onegini.start should exist", function () {
-      expect(onegini.start).toBeDefined();
+    describe('onegini.start', function () {
+      it("onegini.start should exist", function () {
+        expect(onegini.start).toBeDefined();
+      });
     });
   });
 
@@ -16,24 +18,61 @@ exports.defineAutoTests = function () {
       expect(onegini.user).toBeDefined();
     });
 
-    it("onegini.user.startRegistration should exist", function () {
-      expect(onegini.user.startRegistration).toBeDefined();
+    describe('onegini.user.startRegistration', function () {
+      it("onegini.user.startRegistration should exist", function () {
+        expect(onegini.user.startRegistration).toBeDefined();
+      });
     });
 
-    it("onegini.user.createPin should exist", function () {
-      expect(onegini.user.createPin).toBeDefined();
+    describe('onegini.user.createPin', function () {
+      it("onegini.user.createPin should exist", function () {
+        expect(onegini.user.createPin).toBeDefined();
+      });
+
+      it("onegini.user.createPin success cb mandatory", function () {
+        expect(function () {
+          onegini.user.createPin([]);
+        }).toThrow(new TypeError("Onegini: missing success callback for createPin"));
+      });
+
+      it("onegini.user.createPin 'pin' argument mandatory", function () {
+        expect(function () {
+          onegini.user.createPin([], function(){}, function(){});
+        }).toThrow(new TypeError("Onegini: missing 'pin' argument for createPin"));
+      });
+
+      /* This is an example of checking error callbacks, which we'll prolly need in the future
+      it("onegini.user.createPin empty pin invokes error cb", function (done) {
+        onegini.user.createPin(
+            [],
+            function (msg) {
+              expect(msg).toBeUndefined();
+            },
+            function (err) {
+              expect(err).toBeDefined();
+              expect(err.description).toBe("pin is mandatory");
+              done();
+            });
+      });
+      */
     });
 
-    it("onegini.user.getUserProfiles should exist", function () {
-      expect(onegini.user.getUserProfiles).toBeDefined();
+    describe('onegini.user.getUserProfiles', function () {
+      it("onegini.user.getUserProfiles should exist", function () {
+        expect(onegini.user.getUserProfiles).toBeDefined();
+      });
     });
 
-    it("onegini.user.startAuthentication should exist", function () {
-      expect(onegini.user.startAuthentication).toBeDefined();
+    describe('onegini.user.startAuthentication', function () {
+      it("onegini.user.startAuthentication should exist", function () {
+        expect(onegini.user.startAuthentication).toBeDefined();
+      });
     });
 
-    it("onegini.user.checkPin should exist", function () {
-      expect(onegini.user.checkPin).toBeDefined();
+    describe('onegini.user.checkPin', function () {
+      it("onegini.user.checkPin should exist", function () {
+        expect(onegini.user.checkPin).toBeDefined();
+      });
     });
   });
 };
