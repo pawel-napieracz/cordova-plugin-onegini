f8c5c64bcbb31f631470bc87becae8c91e5d4229
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-08-31 10:58:27 +0200

    CORDOPLUG-125 - Let's move this to a background thread to smoothen app start a bit

--- src/ios/classes/OGCDVClient.m
+++ src/ios/classes/OGCDVClient.m
@@ -6,20 +6,23 @@
 
 - (void)start:(CDVInvokedUrlCommand*)command
 {
-  [[ONGClientBuilder new] build];
+  [self.commandDelegate runInBackground:^{
 
-  [[ONGClient sharedInstance] start:^(BOOL result, NSError *error) {
-    if (error != nil) {
-      if (ONGGenericErrorOutdatedApplication == error.code) {
-        [self sendErrorResultForCallbackId:command.callbackId withMessage:@"The application version is no longer valid, please visit the app store to update your application."];
-      }
+    [[ONGClientBuilder new] build];
 
-      if (ONGGenericErrorOutdatedOS == error.code) {
-        [self sendErrorResultForCallbackId:command.callbackId withMessage:@"The operating system that you use is no longer valid, please update your OS."];
+    [[ONGClient sharedInstance] start:^(BOOL result, NSError *error) {
+      if (error != nil) {
+        if (ONGGenericErrorOutdatedApplication == error.code) {
+          [self sendErrorResultForCallbackId:command.callbackId withMessage:@"The application version is no longer valid, please visit the app store to update your application."];
+        }
+
+        if (ONGGenericErrorOutdatedOS == error.code) {
+          [self sendErrorResultForCallbackId:command.callbackId withMessage:@"The operating system that you use is no longer valid, please update your OS."];
+        }
+      } else {
+        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
       }
-    } else {
-      [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
-    }
+    }];
   }];
 }
 
