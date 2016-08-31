c03a77019f761534d9c4b8ab241e16956e6b0a20
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-08-31 09:44:08 +0200

    CORDOPLUG-141 Add Cordova plugin test framework for on-device testing - basic checks added for functions to exist

--- tests/tests.js
+++ tests/tests.js
@@ -1,9 +1,39 @@
 /* jshint jasmine: true */
 
 exports.defineAutoTests = function () {
-  describe('onegini.start', function () {
-    it("should exist", function () {
+  describe('onegini', function () {
+    it("onegini should exist", function () {
+      expect(window.onegini).toBeDefined();
+    });
+
+    it("onegini.start should exist", function () {
       expect(onegini.start).toBeDefined();
+    });
+  });
+
+  describe('onegini.user', function () {
+    it("onegini.user should exist", function () {
+      expect(onegini.user).toBeDefined();
+    });
+
+    it("onegini.user.startRegistration should exist", function () {
+      expect(onegini.user.startRegistration).toBeDefined();
+    });
+
+    it("onegini.user.createPin should exist", function () {
+      expect(onegini.user.createPin).toBeDefined();
+    });
+
+    it("onegini.user.getUserProfiles should exist", function () {
+      expect(onegini.user.getUserProfiles).toBeDefined();
+    });
+
+    it("onegini.user.startAuthentication should exist", function () {
+      expect(onegini.user.startAuthentication).toBeDefined();
+    });
+
+    it("onegini.user.checkPin should exist", function () {
+      expect(onegini.user.checkPin).toBeDefined();
     });
   });
 };
