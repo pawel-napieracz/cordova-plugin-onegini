55788d2363caf85a7ceb50aaa8fc3195eeca2440
Jim van der Voort <jimvandervoort@me.com>
2016-09-13 15:34:11 +0200

    CORDOPLUG-161: Add JS Api for resource fetching.

--- www/onegini.js
+++ www/onegini.js
@@ -2,14 +2,42 @@ module.exports = (function () {
   var user = require('./user');
   var device = require('./device');
   var utils = require('./utils');
+  var resource = require('./resource');
 
-  function start(successCb, failureCb) {
-    return utils.promiseOrCallbackExec('OneginiClient', 'start', [], successCb, failureCb);
+  function start(options, successCb, failureCb) {
+    var promise, callbackResult;
+
+    if (typeof (options) === 'function') {
+      failureCb = successCb;
+      successCb = options;
+      options = undefined;
+    }
+
+    options = options || {};
+
+    if (!successCb) {
+      promise = new Promise(function (resolve, reject) {
+        successCb = resolve;
+        failureCb = reject;
+      });
+    }
+
+    callbackResult = utils.callbackExec('OneginiClient', 'start', [], function (config) {
+          if (options.secureXhr === true) {
+            resource.init(config.resourceBaseURL);
+          }
+          successCb();
+        },
+        failureCb
+    );
+
+    return promise || callbackResult;
   }
 
   return {
     start: start,
     user: user,
-    device: device
+    device: device,
+    resource: resource
   };
 })();
---
+++ www/resource.js
@@ -0,0 +1,96 @@
+module.exports = (function (open) {
+  var utils = require('./utils');
+
+  function fetch(options, successCb, failureCb) {
+    options = options || {};
+
+    if (typeof(options) === 'string') {
+      options = {
+        url: options
+      }
+    }
+
+    options.method = options.method || 'GET';
+    options.headers = options.headers || {};
+    options.body = options.body || '';
+
+    return utils.promiseOrCallbackExec('OneginiResourceClient', 'fetch', options, successCb, failureCb);
+  }
+
+  function init(resourceBaseURL) {
+    XMLHttpRequest.prototype.open = function (method, url) {
+      if (url.startsWith(resourceBaseURL)) {
+        interceptXhr(this, method, url);
+      }
+
+      open.apply(this, arguments);
+    };
+  }
+
+  function interceptXhr(xhr, method, url) {
+    xhr._requestHeaders = {};
+    xhr._responseHeaders = {};
+    xhr._method = method;
+    xhr._url = url;
+
+    xhr.send = function (body) {
+      fetch({
+        method: xhr._method,
+        url: xhr._url,
+        headers: xhr._requestHeaders,
+        body: body
+      }, function (result) {
+        defineProperty(xhr, 'readyState', 4);
+        defineProperty(xhr, 'responseText', result.body);
+        defineProperty(xhr, 'status', result.status);
+        defineProperty(xhr, 'statusText', result.statusText);
+        xhr._responseHeaders = result.headers;
+        triggerEvent(xhr, 'load');
+      });
+    };
+
+    xhr.getAllResponseHeaders = function () {
+      var headersString = '';
+
+      for (var header in xhr._responseHeaders) {
+        headersString += header + ': ' + xhr._responseHeaders[header] + '\n';
+      }
+
+      return headersString;
+    };
+
+    xhr.getResponseHeader = function (header) {
+      return xhr._responseHeaders[header];
+    };
+
+    xhr.setRequestHeader = function (header, value) {
+      xhr._requestHeaders[header] = value;
+    };
+  }
+
+  function triggerEvent(xhr, eventName) {
+    if (xhr.onreadystatechange) {
+      xhr.onreadystatechange();
+    }
+
+    if (xhr['on' + eventName]) {
+      xhr['on' + eventName]();
+    }
+
+    var event = new Event(eventName);
+    xhr.dispatchEvent(event);
+  }
+
+  function defineProperty(object, property, value) {
+    Object.defineProperty(object, property, {
+      writable: true,
+      value: value
+    });
+  }
+
+  return {
+    fetch: fetch,
+    init: init
+  };
+
+})(XMLHttpRequest.prototype.open);
