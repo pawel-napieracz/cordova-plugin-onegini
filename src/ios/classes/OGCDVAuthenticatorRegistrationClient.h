77a03de7efc221483ef7c3848d015b2cdbb7ebe9
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-09-19 12:41:21 +0200

    CORDOPLUG-149 refactored duplicate code

--- src/ios/classes/OGCDVAuthenticatorRegistrationClient.h
+++ src/ios/classes/OGCDVAuthenticatorRegistrationClient.h
@@ -1,14 +1,8 @@
 //  Copyright © 2016 Onegini. All rights reserved.
 
-#import "CDVPlugin+OGCDV.h"
-#import "OneginiSDK.h"
+#import "OGCDVAuthenticationDelegateHandler.h"
 
-// TODO see if we can extract the ONGAuthenticationDelegate & providePin bits (shared with OGCDVUserAuthenticationClient)
-@interface OGCDVAuthenticatorRegistrationClient : CDVPlugin<ONGAuthenticationDelegate>
-
-@property (nonatomic, copy) NSString *authenticationCallbackId;
-@property (nonatomic, copy) NSString *checkPinCallbackId;
-@property (nonatomic) ONGPinChallenge *pinChallenge;
+@interface OGCDVAuthenticatorRegistrationClient : OGCDVAuthenticationDelegateHandler
 
 - (void)start:(CDVInvokedUrlCommand *)command;
 - (void)providePin:(CDVInvokedUrlCommand *)command;
