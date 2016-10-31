f3c59bb85998ed9f65b15a7664b42619dea0fd49
Alex van Poppelen <avanpoppelen@gmail.com>
2016-10-31 10:25:08 +0100

    CORDOPLUG-157 Proper indentation for AuthenticatorRegistrationClient

--- src/ios/classes/OGCDVAuthenticatorRegistrationClient.m
+++ src/ios/classes/OGCDVAuthenticatorRegistrationClient.m
@@ -8,41 +8,41 @@
 
 - (void)registerNew:(CDVInvokedUrlCommand *)command
 {
-  [self.commandDelegate runInBackground:^{
-      ONGUserProfile *user = [[ONGUserClient sharedInstance] authenticatedUserProfile];
-      if (user == nil) {
-        [self sendErrorResultForCallbackId:command.callbackId withMessage:OGCDVPluginErrorKeyNoUserAuthenticated];
-        return;
-      }
-
-      self.authenticationCallbackId = command.callbackId;
-
-      NSDictionary *options = command.arguments[0];
-      NSString *authenticatorId = options[OGCDVPluginKeyAuthenticatorId];
-
-      NSSet<ONGAuthenticator *> *nonRegisteredAuthenticators = [[ONGUserClient sharedInstance] nonRegisteredAuthenticatorsForUser:user];
-      for (ONGAuthenticator *authenticator in nonRegisteredAuthenticators) {
-        if ([authenticator.identifier isEqualToString:authenticatorId]) {
-          [[ONGUserClient sharedInstance] registerAuthenticator:authenticator delegate:self];
-          return;
+    [self.commandDelegate runInBackground:^{
+        ONGUserProfile *user = [[ONGUserClient sharedInstance] authenticatedUserProfile];
+        if (user == nil) {
+            [self sendErrorResultForCallbackId:command.callbackId withMessage:OGCDVPluginErrorKeyNoUserAuthenticated];
+            return;
         }
-      }
 
-      [self sendErrorResultForCallbackId:command.callbackId withMessage:@"Onegini: No authenticator found."];
-  }];
+        self.authenticationCallbackId = command.callbackId;
+
+        NSDictionary *options = command.arguments[0];
+        NSString *authenticatorId = options[OGCDVPluginKeyAuthenticatorId];
+
+        NSSet<ONGAuthenticator *> *nonRegisteredAuthenticators = [[ONGUserClient sharedInstance] nonRegisteredAuthenticatorsForUser:user];
+        for (ONGAuthenticator *authenticator in nonRegisteredAuthenticators) {
+            if ([authenticator.identifier isEqualToString:authenticatorId]) {
+                [[ONGUserClient sharedInstance] registerAuthenticator:authenticator delegate:self];
+                return;
+            }
+        }
+
+        [self sendErrorResultForCallbackId:command.callbackId withMessage:@"Onegini: No authenticator found."];
+    }];
 }
 
 - (void)providePin:(CDVInvokedUrlCommand *)command
 {
-  if (!self.pinChallenge) {
-    [self sendErrorResultForCallbackId:command.callbackId withMessage:@"Onegini: please invoke 'start' first."];
-    return;
-  }
+    if (!self.pinChallenge) {
+        [self sendErrorResultForCallbackId:command.callbackId withMessage:@"Onegini: please invoke 'start' first."];
+        return;
+    }
 
-  self.checkPinCallbackId = command.callbackId;
-  NSDictionary *options = command.arguments[0];
-  NSString *pin = options[OGCDVPluginKeyPin];
-  [self.pinChallenge.sender respondWithPin:pin challenge:self.pinChallenge];
+    self.checkPinCallbackId = command.callbackId;
+    NSDictionary *options = command.arguments[0];
+    NSString *pin = options[OGCDVPluginKeyPin];
+    [self.pinChallenge.sender respondWithPin:pin challenge:self.pinChallenge];
 }
 
 @end
