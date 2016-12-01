8cf4da10caffa19fd6b3f2a18e624d15faf36e05
Jim van der Voort <jimvandervoort@me.com>
2016-12-01 18:05:05 +0100

    CORDOPLUG-159: Resolve iOS Fido SDK if fido plugin is included.

--- hooks/resolve_dependencies.js
+++ hooks/resolve_dependencies.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const spawn = require('child_process').spawn;
@@ -30,7 +30,7 @@ module.exports = function (context) {
     cwd: cwd,
     shell: true
   };
-  executable = getGradleExecutableForPlatform()
+  executable = getGradleExecutableForPlatform();
   const gradle = spawn(executable, ['clean', 'resolveDependencies'], options);
 
   gradle.stdout.on('data', (data) => {
