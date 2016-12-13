842bbe2a5b0228faa900feed1aafef1d236508d8
Jim van der Voort <jimvandervoort@me.com>
2016-12-13 13:21:03 +0100

    EXAMPLECOR-6 Add Dashboard view

---
+++ src/components/Device-list.vue
@@ -0,0 +1,66 @@
+<template>
+  <ul v-if="devices.length > 0" class="block-list">
+    <li v-for="device in devices" class="block">
+      <h5>{{device.name}}</h5>
+      <ul class="prop-list">
+        <li>Application: {{ device.application }}</li>
+        <li>Platform: {{ device.platform }}</li>
+        <li>Mobile authentication: {{ device.mobile_authentication_enabled ? "YES" : "NO" }}</li>
+      </ul
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      devices: []
+    }
+  },
+
+  created: function() {
+    this.fetchDeviceList();
+  },
+
+  methods: {
+    fetchDeviceList: function() {
+      onegini.resource.fetch('https://onegini-msp-snapshot.test.onegini.io/resources/devices')
+          .then((result) => {
+            this.devices = JSON.parse(result.body).devices;
+          })
+          .catch((err) => {
+            navigator.notfication.alert('Error while fetching device list! ' + err.description)
+          });
+    }
+  },
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+}
+
+.block-list {
+  padding: 0 1em;
+}
+
+.block {
+  padding: .5em;
+  margin: 1.2em;
+  background-color: rgba(0, 0, 0, .05);
+  box-shadow: 0 0 5px rgba(0, 0, 0, .3);
+  text-align: left;
+}
+
+h5 {
+  margin: 0 0 .3em 0;
+}
+
+.prop-list {
+  padding: 0;
+  font-size: .8em;
+}
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h3>Hello!</h3>
+    <button-lg text="Settings" />
+    <button-lg text="Logout" @click="logout" />
+    <button-lg text="Deregister" @click="deregister" />
+    <h3>Your devices:</h3>
+    <device-list />
+  </div>
+</template>
+
+<script>
+import ButtonLarge from '../components/Button-large.vue';
+import DeviceList from '../components/Device-list.vue';
+
+export default {
+  components: {
+    'button-lg': ButtonLarge,
+    'device-list': DeviceList
+  },
+
+  methods: {
+    logout: function() {
+      onegini.user.logout()
+          .then(() => {
+            this.$router.push('login')
+          });
+    },
+
+    deregister: function() {
+      onegini.user.getAuthenticatedUserProfile()
+          .then((userProfile) => {
+            return onegini.user.deregister(userProfile);
+          })
+          .then(() => {
+            this.$router.push('login');
+          })
+          .catch((err) => {
+            navigator.notification.alert('Something went wrong! ' + err.description, () => {
+              this.$router.push('login');
+            });
+          });
+    }
+  }
+}
+</script>
