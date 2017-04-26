7e682a16b2b953b485a568fee2d3172d197282e2
Stein Welberg <stein@onegini.com>
2017-04-26 16:35:49 +0200

    CORDOPLUG-221 Update the Copyright statement

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,7 @@
 /* jshint jasmine: true */
 
 /*
- * Copyright (c) 2016 Onegini B.V.
+ * Copyright (c) 2017 Onegini B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
