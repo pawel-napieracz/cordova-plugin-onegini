564da2f6d53d7bafa377851dabc725f43f84d3f1
Marcin Paszylk <marcin@onegini.com>
2017-12-07 13:01:12 +0100

    revert last push

--- tests/tests.js
+++ tests/tests.js
@@ -1588,6 +1588,33 @@ exports.defineAutoTests = function () {
           });
       });
 
+      it('should fetch a resource with a specific body without content-type', function (done) {
+        var body = "foobar";
+        onegini.resource.fetch(
+          {
+            url: 'https://onegini-msp-snapshot.test.onegini.io/resources/mirror-request',
+            method: 'POST',
+            body: body,
+            headers: {
+              'X-Test-String': 'foobar',
+              'X-Test-Int': 1337
+            }
+          },
+          function (response) {
+            expect(response).toBeDefined();
+            expect(response.body).toEqual(body);
+            expect(response.rawBody).toBeDefined();
+            expect(response.headers).toBeDefined();
+            expect(response.headers['Content-Type']).toEqual('text/plain;charset=utf-8');
+            expect(response.status).toEqual(200);
+            expect(response.statusText).toBeDefined();
+            done();
+          }, function (err) {
+            expect(err).toBeUndefined();
+            fail('Error callback called, but method should have succeeded');
+          });
+      });
+
       it('should fetch a resource with a JSON body', function (done) {
         var body = {
           "foo": "oof",
