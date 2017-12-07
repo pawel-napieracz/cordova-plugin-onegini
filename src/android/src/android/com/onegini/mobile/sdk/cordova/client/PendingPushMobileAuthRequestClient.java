83e98cfbd02d4457fd90d7f806a8c726e6d3955d
Robert Szuba <robert@onegini.com>
2017-12-07 17:11:32 +0100

    CORDOPLUG-277 New action and handler for fetching pending transactions (WIP)

---
+++ src/android/src/android/com/onegini/mobile/sdk/cordova/client/PendingPushMobileAuthRequestClient.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017 Onegini B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.onegini.mobile.sdk.cordova.client;
+
+import static com.onegini.mobile.sdk.cordova.OneginiCordovaPluginConstants.ERROR_CODE_PLUGIN_INTERNAL_ERROR;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Context;
+import com.onegini.mobile.sdk.android.handlers.OneginiPendingMobileAuthWithPushRequestsHandler;
+import com.onegini.mobile.sdk.cordova.OneginiSDK;
+import com.onegini.mobile.sdk.cordova.handler.PendingMobileAuthWithPushRequestsHandler;
+import com.onegini.mobile.sdk.cordova.util.ActionArgumentsUtil;
+import com.onegini.mobile.sdk.cordova.util.PluginResultBuilder;
+
+@SuppressWarnings("unused")
+public class PendingPushMobileAuthRequestClient extends CordovaPlugin {
+
+  private static final String ACTION_FETCH = "fetch";
+
+  @Override
+  public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    if (ACTION_FETCH.equals(action)) {
+      fetch(args, callbackContext);
+      return true;
+    }
+
+    return false;
+  }
+
+  private void fetch(final JSONArray args, final CallbackContext callbackContext) {
+    cordova.getThreadPool().execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          final OneginiPendingMobileAuthWithPushRequestsHandler handler = new PendingMobileAuthWithPushRequestsHandler(callbackContext);
+          getOneginiClient().getUserClient().getPendingMobileAuthWithPushRequests(handler);
+        } catch (Exception e) {
+          callbackContext.sendPluginResult(new PluginResultBuilder()
+              .withPluginError(e.getMessage(), ERROR_CODE_PLUGIN_INTERNAL_ERROR)
+              .build());
+        }
+      }
+    });
+  }
+
+  private com.onegini.mobile.sdk.android.client.OneginiClient getOneginiClient() {
+    return OneginiSDK.getInstance().getOneginiClient(getApplicationContext());
+  }
+
+  private Context getApplicationContext() {
+    return cordova.getActivity().getApplicationContext();
+  }
+}
