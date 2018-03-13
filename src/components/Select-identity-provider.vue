cae636014d324710c027118ba6fe50c3b623082b
Marcin Paszylk <marcin@onegini.com>
2018-03-13 14:39:42 +0100

    EXAMPLECOR-21 implement user registration with multiple IdPs

---
+++ src/components/Select-identity-provider.vue
@@ -0,0 +1,45 @@
+<template>
+  <select v-model="selectedIdentityProviderId">
+    <option :value="null">Default identity provider</option>
+    <option :value="identityProvider.id" v-for="identityProvider in identityProviders">{{identityProvider.name}}
+    </option>
+  </select>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        selectedIdentityProviderId: null
+      }
+    },
+
+    props: [
+      'identity-providers'
+    ],
+
+    watch: {
+      selectedIdentityProviderId: function () {
+        this.$emit('select', this.selectedIdentityProviderId);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  select {
+    -webkit-appearance: none;
+    display: block;
+    position: relative;
+    padding: .1em .5em;
+    min-width: 80%;
+    margin: .5em auto;
+
+    color: #00aeef;
+    font-size: 1.1em;
+
+    background-color: transparent;
+    border: 1px solid #00aeef;
+    border-radius: .1em;
+  }
+</style>
